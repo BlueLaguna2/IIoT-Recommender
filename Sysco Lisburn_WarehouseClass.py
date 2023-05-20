f92d72305b11850c7f1c358e52d59b841307d3b3
Patrick Daly <dalyp5592@gmail.com>
2023-05-20 16:25:57 +0100

    Uploaded on 20-May-2023 before Thesis submission
    Uploaded on 20-May-2023 before Thesis submission

--- Sysco Lisburn_WarehouseClass.py
+++ Sysco Lisburn_WarehouseClass.py
@@ -1,4 +1,23 @@
 class Warehouse:
     def __init__(self, location):
          self.location = location
+         self.pallets = {}
  
+    def add_pallet(self, pallet_id, product_name, quantity):
+         if pallet_id not in self.pallets:
+             self.pallets[pallet_id] = (product_name, quantity)
+             self.add_product(product_name, quantity)
+         else:
+             print('Error: Pallet ID already exists') 
+ 
+    def remove_pallet(self, pallet_id):
+         if pallet_id in self.pallets:
+             product_name, quantity = self.pallets[pallet_id]
+             del self.pallets[pallet_id]
+             self.remove_product(product_name, quantity)
+         else:
+             print('Error: Pallet ID does not exist')
+ 
+oduct_name]
+          else:
+              return 0
